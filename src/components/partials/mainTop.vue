454507857e3f3ca252c183f0f39575404e6139fe
ClaudioLeone <claleone92@gmail.com>
2023-04-26 23:36:04 +0200

    Completed 'middle' section of 'mainTop' component

--- src/components/partials/mainTop.vue
+++ src/components/partials/mainTop.vue
@@ -11,11 +11,10 @@ export default {
 
 <template>
     <div class="main-top container">
-
         <div class="upper d-flex justify-content-around">
             <div class="d-flex flex-column upper-text-sect justify-content-center">
                 <img class="books-icon" src="../../assets/edu/img/Books-icon.png" alt="">
-                <h4>Thousands of courses for any type of student</h4>
+                <h3>Thousands of courses for any type of student</h3>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi mollitia, ducimus, quos eaque cumque et, modi eveniet ad neque maxime alias. Non delectus tenetur accusantium accusamus asperiores est obcaecati sapiente.</p>
                 <button><a href="#">Browse through courses</a></button>
             </div>
@@ -24,18 +23,39 @@ export default {
                 <img class="graduation-illustration" src="../../assets/edu/img/Graduation-Illustration.png" alt="Graduation-Illustration">
             </div>
         </div>
+
+        <div class="middle d-flex flex-column justify-content-center align-items-center">
+            <h3>Faculties available at EduPrime</h3>
+
+            <div class="middle-text-sect">
+                <p>A single university with a load of courses, tailored to satisfy any student's needs.</p>
+            </div>
+            
+            <div class="row">
+                <div class="col d-flex flex-column align-items-center justify-content-center"><span>Law Faculty</span></div>
+                <div class="col d-flex flex-column align-items-center justify-content-center"><span>Economy</span></div>
+                <div class="col d-flex flex-column align-items-center justify-content-center"><span>Medicine</span></div>
+                <div class="col d-flex flex-column align-items-center justify-content-center"><span>Computer Science</span></div>
+                <div class="col d-flex flex-column align-items-center justify-content-center"><span>Graphic Design</span></div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-.main-top {
-    height: 600px;
-}
 .books-icon {
     width: 30px;
     position: absolute;
     left: 0;
     top: -5px;
+}
+
+h3 {
+    font-weight: bold;
+}
+
+p {
+    font-size: 0.8rem;
 }
 
 .upper {
@@ -46,10 +66,6 @@ export default {
     width: 30%;
     p {
         font-size: 0.7rem;
-    }
-
-    h4 {
-        font-weight: bold;
     }
 
     button {
@@ -75,9 +91,39 @@ export default {
 }
 
 .grad-ill-container {
-    width: 40%;
+    width: 35%;
     .graduation-illustration {
     width: 100%;
 }
 }
+.middle {
+    text-align: center;
+    padding: 35px 0;
+}
+.middle-text-sect {
+    width: 30%;
+    padding: 20px 0;
+}
+.row {
+    width: 100%;
+    height: 8rem;
+
+    .col {
+        width: calc(100% / 5);
+        color: #B75253;
+        font-weight: bold;
+        cursor: pointer;
+        border: 1px solid #e5e5e5;
+
+        span {
+            font-size: 0.7rem;
+        }
+    }
+
+    .col:hover {
+        color: white;
+        background-color: #B75253;
+    }
+}
+
 </style>
